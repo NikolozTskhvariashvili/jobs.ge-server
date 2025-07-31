b26f28b1c89151a48a1763d7dc9775af75715886
nikoloz <jdjjnxxj581@gmail.com>
2025-07-31 23:49:42 +0400

    vercel test

--- src/aws-s3/aws-s3.service.ts
+++ src/aws-s3/aws-s3.service.ts
@@ -16,7 +16,7 @@ export class AwsS3Service {
     this.s3 = new S3Client({
       credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-        secretAccessKey: process.env.AWS_SECRET_KEY!,
+        secretAccessKey: process.env.AWS_SECRET_KEY_ID!,
       },
       region: process.env.AWS_REGION,
     });
