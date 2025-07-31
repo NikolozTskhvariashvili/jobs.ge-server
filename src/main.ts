02113e2d2057b065046027b66cde5c1dda5da0d7
nikoloz <jdjjnxxj581@gmail.com>
2025-07-31 23:03:18 +0400

    remove cred

--- src/main.ts
+++ src/main.ts
@@ -14,9 +14,8 @@ async function bootstrap() {
   app.enableCors({
     origin: ['http://localhost:3000', process.env.FRONT_URL],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
   });
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
